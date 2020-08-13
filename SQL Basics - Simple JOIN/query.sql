02f455d0e6593a93364c9344e41546e6a8336424
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-13 18:34:15 +0200

    Create query.sql

---
+++ SQL Basics - Simple JOIN/query.sql
@@ -0,0 +1,8 @@
+-- https://www.codewars.com/kata/5802e32dd8c944e562000020/train/sql
+
+SELECT    
+  p.*,
+  c.name AS company_name
+FROM products AS p
+	LEFT JOIN companies AS c
+	ON p.company_id = c.id;
