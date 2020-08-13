dc367ef74570e37275ca53f2d7835eda1779162d
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-13 18:32:19 +0200

    Create query.sql

---
+++ SQL - Regex AlphaNumeric Split/query.sql
@@ -0,0 +1,7 @@
+-- https://www.codewars.com/kata/594257d4db68b6e99200002c/train/sql
+
+SELECT
+  project,
+  regexp_replace(address,'[0-9]','','g') as letters,
+  regexp_replace(address,'[a-zA-Z]','','g') as numbers 
+FROM repositories;
