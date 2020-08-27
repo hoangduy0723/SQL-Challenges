a4eb00f731373e5556c5b95262ac205451f53b18
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-27 20:30:14 +0200

    add solution to Hackerrank challenges

--- HackerRank-SQL/Challenges/Binary_Tree_Nodes.sql
+++ HackerRank-SQL/Challenges/Binary_Tree_Nodes.sql
@@ -9,6 +9,8 @@
  * If the answer is no, then n is a root.
  */
  
+
+ 
 SELECT N, 
 CASE WHEN P IS NULL THEN "Root"
 WHEN NOT EXISTS (SELECT inner_table.P FROM BST AS inner_table WHERE inner_table.P = outer_table.N) THEN "Leaf"
